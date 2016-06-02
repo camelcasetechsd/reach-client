62e0c1bc8b613bf630273d8269021f8ba1b4e785
Mohamed Labib <lebaz20@gmail.com>
2016-06-02 19:28:04 +0200

    Add localForage storage for redux to keep data stored after refresh - Although on first refresh before pressing next, data is not present

--- application/components/utils/store/create-store.js
+++ application/components/utils/store/create-store.js
@@ -13,6 +13,10 @@ import promiseMiddleware from './promise-middleware'
 // ./reducers.js to see what our reducer actually do (no magic there).
 import * as reducers from './reducers'
 
+// Enable data persistance into local storage using localForage
+import {persistStore, autoRehydrate} from 'redux-persist'
+import localForage from 'localforage'
+
 // The data parameter that we see here is used to initialize our redux store with data. We didn't
 // talk about this yet for simplicity but thanks to it your reducers can be initialized
 // with real data if you already have some. For example in an isomorphic/universal app where you
@@ -20,9 +24,10 @@ import * as reducers from './reducers'
 // initialized with that data.
 // We're not passing any data here but it's good to know about this createStore's ability.
 export default function(data) {
-  var reducer = combineReducers(reducers)
-  var finalCreateStore = applyMiddleware(promiseMiddleware)(createStore)
-  var store = finalCreateStore(reducer, data)
 
-  return store
+	var reducer = combineReducers(reducers)
+	var finalCreateStore = applyMiddleware(promiseMiddleware)(createStore)
+	var store = finalCreateStore(reducer, data, autoRehydrate())
+	persistStore(store, {storage: localForage});
+	return store
 }
